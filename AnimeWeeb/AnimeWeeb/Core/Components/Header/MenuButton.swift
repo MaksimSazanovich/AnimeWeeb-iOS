f6c93accc9b7d1bed245b0fae412835d3e4bf21a
Unity_one_love <sazanovichmaksim@gmail.com>
2026-08-02 18:55:27 +0300

    feat(ui): add Menu Button

---
+++ AnimeWeeb/AnimeWeeb/Core/Components/Header/MenuButton.swift
@@ -0,0 +1,26 @@
+//
+//  MenuButton.swift
+//  AnimeWeeb
+//
+
+import SwiftUI
+
+struct MenuButton: View {
+    var body: some View {
+        Button {
+            print("Menu Button pressed")
+        } label: {
+            ZStack {
+                Image(systemName: "line.horizontal.3")
+                    .font(.system(size: 22, weight: .semibold))
+                    .foregroundStyle(Color(.menuIcon))
+            }
+            .frame(width: 38, height: 38)
+            .overlay(
+                RoundedRectangle(cornerRadius: 8)
+                    .strokeBorder(.menuButtonStroke, lineWidth: 1))
+            .clipShape(RoundedRectangle(cornerRadius: 8))
+        }
+
+    }
+}
