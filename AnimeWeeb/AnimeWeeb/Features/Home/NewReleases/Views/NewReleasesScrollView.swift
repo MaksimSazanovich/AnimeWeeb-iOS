94cd8c85d0a61ecc0e8e747fc2097c1aadb7413c
Unity_one_love <sazanovichmaksim@gmail.com>
2026-08-03 18:16:53 +0300

    refactor(ui): extract NewReleases Scroll View

---
+++ AnimeWeeb/AnimeWeeb/Features/Home/NewReleases/Views/NewReleasesScrollView.swift
@@ -0,0 +1,25 @@
+//
+//  NewReleasesScrollView.swift
+//  AnimeWeeb
+//
+
+import SwiftUI
+
+struct NewReleasesScrollView: View {
+    
+    var animes: [NewReleasesAnimeModel]
+    
+    var body: some View {
+        ScrollView(.horizontal) {
+            LazyHStack(spacing: 16) {
+                ForEach(animes) { anime in
+                    NewReleasesAnimeCard(model: anime)
+                }
+            }
+            .padding(.bottom, 30)
+        }
+        
+    }
+}
+
+
--- AnimeWeeb/AnimeWeeb/Features/Home/Views/HomeScreen.swift
+++ AnimeWeeb/AnimeWeeb/Features/Home/Views/HomeScreen.swift
@@ -10,25 +10,40 @@ struct HomeScreen: View {
     var animes: [NewReleasesAnimeModel]
     
     var body: some View {
-
+        
         ZStack(alignment: .top) {
             
             
             ScrollView {
-                LazyVStack(alignment: .leading) {
-                    
+                VStack(alignment: .leading) {
+                    // MARK: - New Releases
                     Text("Новое на сайте")
                         .font(.system(.largeTitle, weight: .semibold))
                         .foregroundStyle(.largeTitle)
                     
-                    ScrollView(.horizontal) {
-                        LazyHStack(spacing: 16) {
-                            ForEach(animes) { anime in
-                                NewReleasesAnimeCard(model: anime)
-                            }
+                    VStack(alignment: .leading, spacing: 32) {
+                        
+                        VStack(spacing: 50) {
+                            //MARK: New Releases ScrollView
+                            NewReleasesScrollView(animes: animes)
+                            
+                            Rectangle()
+                                .fill(.stroke.opacity(0.8))
+                                .frame(height: 1)
                         }
-                        .padding(.bottom, 30)
+                        
+                        VStack(alignment: .leading) {
+                            Text("Каталог")
+                                .font(.system(.largeTitle, weight: .semibold))
+                                .foregroundStyle(.largeTitle)
+                            
+                            Text("От ценителей — для ценителей!")
+                                .font(.subheadline)
+                                .foregroundColor(.subtitle)
+                        }
+                        
                     }
+                    
                     
                 }
                 .frame(maxWidth: .infinity, alignment: .leading)
@@ -36,7 +51,7 @@ struct HomeScreen: View {
                 .padding(.horizontal)
             }
             .safeAreaInset(edge: .top) {
-                // MARK: App Header
+                // MARK: - App Header
                 AppHeaderView()
             }
         }
