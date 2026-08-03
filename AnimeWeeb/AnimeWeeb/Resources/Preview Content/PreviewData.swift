3108523ee3f1ac5090b0d0b35d463218cd085baf
Unity_one_love <sazanovichmaksim@gmail.com>
2026-08-03 16:16:37 +0300

    feat(model): add NewReleases PreviewData

---
+++ AnimeWeeb/AnimeWeeb/Resources/Preview Content/PreviewData.swift
@@ -0,0 +1,10 @@
+//
+//  PreviewData.swift
+//  AnimeWeeb
+//
+
+import Foundation
+
+var previewNewReleasesAnimeModel = NewReleasesAnimeModel(imageURL: URL(string: "https://shikimori.io/uploads/poster/animes/35507/main_alt-528a79ed0d5f22e1c954df240efe63c3.jpeg"), title: "Добро пожаловать в класс превосходства", subtitle: "Добро пожаловать в класс превосходства", season: 4, episode: 13)
+
+var previewNewReleasesAnimeModels: [NewReleasesAnimeModel] = Array(repeating: previewNewReleasesAnimeModel, count: 10)
