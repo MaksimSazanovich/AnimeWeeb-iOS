11f8c86291cfc539c0f5250b45c0224b1b1b05a6
Unity_one_love <sazanovichmaksim@gmail.com>
2026-08-03 16:16:02 +0300

    feat(model): add NewReleasesAnimeModel

---
+++ AnimeWeeb/AnimeWeeb/Features/Home/NewReleases/Models/NewReleasesAnimeModel.swift
@@ -0,0 +1,16 @@
+//
+//  NewReleasesAnimeModel.swift
+//  AnimeWeeb
+//
+
+import Foundation
+
+struct NewReleasesAnimeModel: Identifiable {
+    let id = UUID()
+    
+    let imageURL: URL?
+    let title: String
+    let subtitle: String
+    let season: Int
+    let episode: Int
+}
