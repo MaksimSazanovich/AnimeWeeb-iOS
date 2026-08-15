ee9d5cc1a7411bc8e166d06bde39d457aa0fb4af
Unity_one_love <sazanovichmaksim@gmail.com>
2026-08-15 13:52:36 +0300

    feat(ui): add Skeleton AnimeDetails ContentView

--- AnimeWeeb/AnimeWeeb/Features/AnimeDetails/Views/AnimeDetailsScreen.swift
+++ AnimeWeeb/AnimeWeeb/Features/AnimeDetails/Views/AnimeDetailsScreen.swift
@@ -15,50 +15,17 @@ struct AnimeDetailsScreen: View {
     
     var body: some View {
         
-        ScrollView {
-            VStack(spacing: 32) {
-                // MARK: Image
-                LazyImage(url: viewModel.imageURL) { state in
-                    if let image = state.image {
-                        image
-                            .resizable()
-                            .scaledToFill()
-                            .clipShape(
-                                RoundedRectangle(cornerRadius: 12)
-                            )
-                    }
-                }
-                .animeCardBackgroundModifier(cornerRadius: 12, strokeOpacity: 1)
-                .padding(.horizontal)
+        ZStack {
+            switch viewModel.state {
+            case .idle, .loading:
+                SkeletonAnimeDetailsContentView()
+            
+            case .loaded:
+                AnimeDetailsContentView(viewModel: viewModel)
                 
-                VStack(alignment: .leading, spacing: 12) {
-                    // MARK: Title
-                    Text(viewModel.title)
-                        .font(.system(size: 30, weight: .semibold))
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    
-                    // MARK: Tag Cloud
-                    TagCloudView(data: viewModel.genres) { genre in
-                        Text(genre.rawValue.uppercased())
-                            .font(.system(.body, weight: .medium))
-                            .foregroundStyle(.subtitle)
-                            .padding(.horizontal, 12)
-                            .padding(.vertical, 4)
-                            .background(
-                                RoundedRectangle(cornerRadius: 8)
-                                    .fill(.stroke.opacity(0.8))
-                            )
-                    }
-                    
-                    // MARK: Description
-                    Text(viewModel.despription)
-                        .font(.system(.body))
-                        .foregroundStyle(.subtitle)
-                }
+            case .empty, .failed(_):
+                EmptyView()
             }
-            .frame(maxWidth: .infinity)
-            .padding(.top, 50)
-            .padding(.horizontal)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color.background)
---
+++ AnimeWeeb/AnimeWeeb/Features/AnimeDetails/Views/SkeletonAnimeDetailsContentView.swift
@@ -0,0 +1,66 @@
+//
+//  SkeletonAnimeDetailsContentView.swift
+//  AnimeWeeb
+//
+//  Created by Maksim Sazanovich
+//
+
+import SwiftUI
+import Shimmer
+
+struct SkeletonAnimeDetailsContentView: View {
+    
+    
+    var body: some View {
+        
+        ScrollView {
+            VStack(spacing: 32) {
+                // MARK: Image
+                RoundedRectangle(cornerRadius: 12)
+                    .frame(width: 320, height: 480)
+                    .skeletonColor()
+                    .padding(.horizontal)
+                    .shimmering()
+                
+                VStack(alignment: .leading, spacing: 12) {
+                    // MARK: Title
+                    RoundedRectangle(cornerRadius: 7, style: .continuous)
+                        .skeletonColor()
+                        .frame(maxWidth: .infinity, minHeight: 36)
+                    
+                    // MARK: Tag Cloud
+                    HStack {
+                        ForEach(0...2, id: \.self) { _ in
+                            RoundedRectangle(cornerRadius: 8)
+                                .frame(width: 80, height: 24)
+                                .skeletonColor()
+                        }
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    // MARK: Description
+                    RoundedRectangle(cornerRadius: 7, style: .continuous)
+                        .skeletonColor()
+                        .frame(maxWidth: .infinity, minHeight: 24)
+                    
+                    RoundedRectangle(cornerRadius: 7, style: .continuous)
+                        .skeletonColor()
+                        .frame(maxWidth: .infinity, minHeight: 24)
+                        .padding(.trailing, 30)
+                }
+            }
+            .frame(maxWidth: .infinity)
+            .padding(.top, 50)
+            .padding(.horizontal)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.background)
+        .shimmering()
+    }
+}
+
+#Preview {
+    SkeletonAnimeDetailsContentView()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.background)
+}
